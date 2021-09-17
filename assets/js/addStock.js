1ec2a4c14fdd0903d59000a49738b0d4273e65db
FabianoSPFC <programador.workspace@gmail.com>
2021-09-17 19:41:03 -0300

    Add files via upload

---
+++ assets/js/addStock.js
@@ -0,0 +1,41 @@
+const product = document.querySelector(".row");
+
+function createProduct([img, category, tittle, description, price]){
+    let code = `
+    <div class="col-xs-12 col-sm-3 teste">
+    <div class="card">
+    <a class="img-card">
+    <img src="${img}" alt="${category}"/>
+  </a>
+    <div class="card-content">
+        <h4 class="card-title">
+            <a> ${tittle}
+          </a>
+        </h4>
+        <p class="">
+        ${description}
+        </p>
+        <p class="">
+        Preço: R$${price}
+        </p>
+    </div>
+    <div class="card-read-more">
+        <a href="#" class="btn btn-link btn-block">
+            Editar Produto
+        </a>
+    </div>
+</div>
+</div>
+`;
+
+product.innerHTML += code;
+}
+let = a = ["https://assets.dmagstatic.com/wp-content/uploads/media/3151226/LaComida_guacamole.jpg?cropUpAlias=wide_8-12&width=677",
+           "Comida",
+           "Salada",
+           "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+           "10,00"
+          ]
+
+
+createProduct(a);
