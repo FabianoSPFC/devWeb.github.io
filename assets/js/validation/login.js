8752a1083b00bcfcc1c1533a4b717c00de2c4f0a
Rodrigo Melo <drigommelo@hotmail.com>
2021-09-20 21:37:30 -0300

    Alterações Login

--- assets/js/validation/login.js
+++ assets/js/validation/login.js
@@ -1,5 +1,20 @@
 const form = document.querySelector("form");
 const [email, password] = document.querySelectorAll("form input");
+const message = document.querySelector(".invalidPassword");
+/**
+ *
+ * @param {SubmitEvent} e
+ * @param {string} message
+ * @param {number} timeout
+ */
+function displayError(e, messageString, timeout = 10000) {
+  e.preventDefault();
+  message.innerHTML = messageString;
+  message.hidden = false;
+  setTimeout(() => {
+    message.hidden = true;
+  }, timeout);
+}
 
 /**
  *
@@ -12,14 +27,14 @@ function submit(e) {
       password.value
     )
   ) {
-    e.preventDefault();
+    displayError(e, "Senha Incorreta");
   }
   if (
     !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
       email.value
     )
   ) {
-    e.preventDefault();
+    displayError(e, "E-mail Inválido");
   }
 }
 
